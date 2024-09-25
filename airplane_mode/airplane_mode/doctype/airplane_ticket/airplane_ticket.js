34b83f491e7e85b6b95f4335d454747c9f197fd2
kcashique <ashiquekc97@gmail.com>
2024-09-25 16:06:54 +0530

    Airplane Ticket : added a custom action button to assign seat and implimented js script

--- airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.js
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.js
@@ -1,8 +1,30 @@
 // Copyright (c) 2024, ashique and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Airplane Ticket", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Airplane Ticket", {
+    refresh(frm) {
+        frm.add_custom_button(__('Assign Seat'), function() {
+            // Show a dialog to input seat number
+            let d = new frappe.ui.Dialog({
+                title: __('Assign Seat Number'),
+                fields: [
+                    {
+                        label: 'Seat Number',
+                        fieldname: 'seat_number',
+                        fieldtype: 'Data',
+                        reqd: 1
+                    }
+                ],
+                primary_action_label: 'Assign',
+                primary_action(values) {
+                    // Set the seat field in the form
+                    frm.set_value('seat', values.seat_number);
+                    d.hide();
+                }
+            });
+            
+            // Show the dialog
+            d.show();
+        }, __("Actions"));
+    }
+});
