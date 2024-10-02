ffa4c2b75f2465c202ae6e39f87e6aed338406dd
kcashique <ashiquekc97@gmail.com>
2024-10-02 16:43:03 +0530

    feat: fixes

--- airplane_mode/airplane_mode/doctype/airline/airline.js
+++ airplane_mode/airplane_mode/doctype/airline/airline.js
@@ -1,11 +1,16 @@
 // Copyright (c) 2024, ashique and contributors
 // For license information, please see license.txt
+frappe.ui.form.on('Airline', {
+    refresh(frm) {
+        // Check if the Website field is not empty
+        if (frm.doc.website) {
+            // Add the web link to the form header
+            frm.add_web_link(__('Official Website'), frm.doc.website);
 
-frappe.ui.form.on("Airline", {
-	refresh(frm) {
-        if (frm.doc.website){
+            // Add the web link to the dashboard section
             frm.dashboard.add_section('<a href="' + frm.doc.website + '" target="_blank">' + __('Visit Official Website') + '</a>');
         }
-
-	},
+    }
 });
+
+
